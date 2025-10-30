df1faff854763527bb812fbdb76d353c4d2f0e0f
SyahranFD <fadhilrafa1@gmail.com>
2025-10-31 03:24:23 +0700

    feat: [SF] fix bug refresh token

--- src/services/playlistService.js
+++ src/services/playlistService.js
@@ -1,20 +1,12 @@
 // src/services/playlistService.js
-import axios from 'axios';
 import { API_BASE_URL } from '../utils/utils.js';
+import { useAuth } from '../composables/useAuth.js';
 
 export const playlistService = {
   async createPlaylist(pre_mood, phq9) {
     try {
-      const token = localStorage.getItem('access_token');
-      if (!token) {
-        throw new Error('Token tidak tersedia');
-      }
-
-      const response = await axios.get(`${API_BASE_URL}/api/playlists/create?pre_mood=${pre_mood}&phq9=${phq9}`, {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      });
+      const { api } = useAuth();
+      const response = await api.get(`${API_BASE_URL}/api/playlists/create?pre_mood=${pre_mood}&phq9=${phq9}`);
 
       return response.data;
     } catch (error) {
